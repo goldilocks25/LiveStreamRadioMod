ca32b18c300db47eb31065b8f5f908cdc6dff5eb
KJH <baksa0320@naver.com>
2024-04-05 20:02:12 +0900

    채널별 보정치 변경

--- KoreaLiveStreamRadio/Mono/RadioController.cs
+++ KoreaLiveStreamRadio/Mono/RadioController.cs
@@ -54,6 +54,7 @@ public class RadioController : MonoBehaviour
             string liveStation = null;
             string liveChannel = null;
             string streamChannel = null;
+            var conversion = 0.65f;
             
             switch (channel.name)
             {
@@ -61,76 +62,89 @@ public class RadioController : MonoBehaviour
                     liveStation = "mbc";
                     liveChannel = "chm";
                     streamChannel = "chm";
+                    conversion = 0.06f;
                     break;
                 case "MBC standard fm":
                     liveStation = "mbc";
                     liveChannel = "sfm";
                     streamChannel = "sfm";
+                    conversion = 0.06f;
                     break;
                 case "MBC fm 4u":
                     liveStation = "mbc";
                     liveChannel = "fm4u";
                     streamChannel = "mfm";
+                    conversion = 0.06f;
                     break;
                 case "SBS love fm":
                     liveStation = "sbs";
                     liveChannel = "lovefm";
                     streamChannel = "lovefm";
+                    conversion = 0.05f;
                     break;
                 case "SBS power fm":
                     liveStation = "sbs";
                     liveChannel = "powerfm";
                     streamChannel = "powerfm";
+                    conversion = 0.0375f;
                     break;
                 case "SBS gorilradio m":
                     liveStation = "sbs";
                     liveChannel = "dmb";
                     streamChannel = "dmb";
+                    conversion = 0.0375f;
                     break;
                 case "KBS 1 radio":
                     liveStation = "kbs";
                     liveChannel = "1radio";
                     streamChannel = "1radio";
+                    conversion = 0.055f;
                     break;
                 case "KBS 2 radio":
                     liveStation = "kbs";
                     liveChannel = "2radio";
                     streamChannel = "2radio";
+                    conversion = 0.055f;
                     break;
                 case "KBS 3 radio":
                     liveStation = "kbs";
                     liveChannel = "3radio";
                     streamChannel = "3radio";
+                    conversion = 0.055f;
                     break;
                 case "KBS 1 FM radio":
                     liveStation = "kbs";
                     liveChannel = "1fm";
                     streamChannel = "1fm";
+                    conversion = 0.06f;
                     break;
                 case "KBS 2 FM radio":
                     liveStation = "kbs";
                     liveChannel = "2fm";
                     streamChannel = "2fm";
+                    conversion = 0.06f;
                     break;
                 case "YTN radio":
                     liveStation = "ytn";
                     liveChannel = "";
                     streamChannel = "";
+                    conversion = 0.06f;
                     break;
                 case "CBS music fm radio":
                     liveStation = "cbs";
                     liveChannel = "mfm";
                     streamChannel = "mfm";
+                    conversion = 0.06f;
                     break;
             }
-
+            var liveStreamRadio = new LiveStreamRadio(liveStation, liveChannel, streamChannel, conversion);
             if (liveStation == null) return;
 
             if (_isPlaying) return;
             _isPlaying = true;
             LOG.Info($"start radio: {channel.name}");
             StopCoroutine("Play");
-            StartCoroutine(Play(liveStation, liveChannel, streamChannel, channel.name));
+            StartCoroutine(Play(liveStreamRadio, channel.name));
         }
         else
         {
@@ -146,12 +160,12 @@ public class RadioController : MonoBehaviour
         _isPaused = false;
     }
 
-    private IEnumerator ConvertAndPlayAudio(string streamServer)
+    private IEnumerator ConvertAndPlayAudio(string streamServer, float correction)
     {
         var aacUrl = streamServer + ReplaceNumbers(_aacUrl, _aacUrlParamNumber++.ToString());
         LOG.Info($"convert start: {aacUrl}");
         yield return ThreadPool.QueueUserWorkItem(_ => ConvertAccToWavFile(aacUrl, 0));
-        yield return StartCoroutine(PlayAudio());
+        yield return StartCoroutine(PlayAudio(correction));
     }
 
     private IEnumerator DownloadAndParsePls(string streamServer, string hlsServer)
@@ -180,14 +194,16 @@ public class RadioController : MonoBehaviour
         yield return StartCoroutine(DownloadPls(streamServer, m3U8File));
     }
 
-    private IEnumerator Play(string liveStation, string liveChannel, string streamChannel, string gameChannel)
+    private IEnumerator Play(LiveStreamRadio liveStreamRadio, string gameChannel)
     {
-        yield return StartCoroutine(DownloadAndParsePls(
-            RadioStreamServer.GetStreamServer(liveStation, streamChannel), RadioStreamServer.GetHlsServerUrl(liveStation, liveChannel))
+        var streamServer =
+            RadioStreamServer.GetStreamServer(liveStreamRadio.LiveStation, liveStreamRadio.StreamChannel);
+        yield return StartCoroutine(DownloadAndParsePls(streamServer,
+            RadioStreamServer.GetHlsServerUrl(liveStreamRadio.LiveStation, liveStreamRadio.LiveChannel))
         );
         while (_selectionChannel == gameChannel)
         {
-            yield return StartCoroutine(ConvertAndPlayAudio(RadioStreamServer.GetStreamServer(liveStation, streamChannel)));
+            yield return StartCoroutine(ConvertAndPlayAudio(streamServer, liveStreamRadio.Correction));
         }
         _isPlaying = false;
     }
@@ -255,7 +271,7 @@ public class RadioController : MonoBehaviour
             if (!match.Success) continue;
             
             _aacUrl = lines[++i].Trim();
-            _aacUrlParamNumber = ExtractNumbers(_aacUrl) - 2;
+            _aacUrlParamNumber = ExtractNumbers(_aacUrl);
             break;
         }
     }
@@ -272,7 +288,7 @@ public class RadioController : MonoBehaviour
         return input.Substring(0, match.Index) + newNumbers+ "." + input.Substring(match.Index + match.Length);
     }
 
-    private IEnumerator PlayAudio()
+    private IEnumerator PlayAudio(float correction)
     {
         yield return new WaitWhile(() => _isConverting);
         var www = UnityWebRequestMultimedia.GetAudioClip(GetWavFileName(0), AudioType.WAV);
@@ -297,11 +313,8 @@ public class RadioController : MonoBehaviour
             if (!AudioSource.isPlaying)
                 return false;
             
-            var clipLength = AudioSource.clip.length;
-            var correction = clipLength < 4 ? 0.06f : 0.065f;
-            
             // Missing buffer after conversion to wave file
-            return AudioSource.time < clipLength - correction;
+            return AudioSource.time < AudioSource.clip.length - correction;
         });
         
         if (_isPaused)
@@ -353,6 +366,34 @@ public class RadioController : MonoBehaviour
     private static string GetFileName(int index, string format)
     {
         return $"{Application.persistentDataPath}/radio_{index}.{format}";
+    }
+
+    private class LiveStreamRadio
+    {
+        public string LiveStation
+        {
+            get;
+        }
+        public string LiveChannel
+        {
+            get;
+        }
+        public string StreamChannel
+        {
+            get;
+        }
+        public float Correction
+        {
+            get;
+        }
+
+        internal LiveStreamRadio(string liveStation, string liveChannel, string streamChannel, float correction)
+        {
+            LiveStation = liveStation;
+            LiveChannel = liveChannel;
+            StreamChannel = streamChannel;
+            Correction = correction;
+        }
     }
 
 }
